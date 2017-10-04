b17611e6be908c51824d2b1ee9197f043eefb3fd
mcmxc <al.voloshchenko@gmail.com>
2017-10-04 10:32:16 +0300

    missing carsList folder added

---
+++ hw2/src/app/pages/home/carsList/carsList.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carsList',
+  templateUrl: './carsList.component.html',
+  styleUrls: ['./carsList.component.scss']
+})
+export class CarsListComponent {}
