206f7ee0ab60a1d6c3fa5ec68b82934655c53126
mcmxc <al.voloshchenko@gmail.com>
2017-10-10 08:35:08 +0300

    unnecessary comments removed

--- hw3/src/app/models/tab.model.ts
+++ hw3/src/app/models/tab.model.ts
@@ -1,13 +1,7 @@
 export class Tab {
-  constructor(
-    public label: string,
-    public header: string,
-    public content: string,
-    // public isActive: boolean
-  ) {
+  constructor(public label: string, public header: string, public content: string) {
     this.label = label;
     this.content = content;
     this.header = header;
-    // this.isActive = isActive;
   }
 }
