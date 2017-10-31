27992b9211cc808a4a6db83613192ef54b3c5aac
mcmxc <al.voloshchenko@gmail.com>
2017-10-31 18:46:07 +0200

    very raw implementation for horizontal

--- hw6/src/app/splitter/splitter-spot.directive.ts
+++ hw6/src/app/splitter/splitter-spot.directive.ts
@@ -1,43 +1,65 @@
 import {
   Directive,
   ElementRef,
+  EventEmitter,
   HostBinding,
   HostListener,
-  OnInit
+  Input,
+  OnInit,
+  Output
 } from '@angular/core';
 
 @Directive({
   selector: '[splitter-spot]'
 })
 export class SplitterSpotDirective implements OnInit {
-  target: HTMLElement;
-
+  private target: HTMLElement;
   private isMoving = false;
+  private moveFrom: 'top' | 'left';
 
-  constructor(private elementRef: ElementRef) {
-    this.target = elementRef.nativeElement;
-  }
-
+  @Input('options') options;
   @HostBinding('class.splitter-spot') cls = true;
-
   @HostListener('mousedown')
   onMouseDown() {
     this.isMoving = true;
   }
+  ngOnInit() {
+    this.moveFrom = this.target.classList.contains('vertical') ? 'top' : 'left';
+    this.adjustSplitterPosition(this.moveFrom, 200);
+    document.body.addEventListener('mousemove', this.onMouseMove.bind(this));
+    document.body.addEventListener('mouseup', this.onMouseUp.bind(this));
+  }
+  constructor(private elementRef: ElementRef) {
+    this.target = elementRef.nativeElement;
+  }
 
-  onMouseMove(e) {
+  adjustSplitterPosition(dir: string, val: number) {
+    const panel = <HTMLElement>document.querySelector('.panel-1');
+    this.target.style[dir] = val + 'px';
+    panel.style[dir === 'top' ? 'height' : 'width'] = val + 'px';
+    let c = document.querySelector('.container').getBoundingClientRect();
+    if (val < c.left - 80) {
+      this.target.style[dir] = '0px';
+      panel.style[dir === 'top' ? 'height' : 'width'] = '0px';
+    }
+    if (val > c.right - 90) {
+      this.target.style[this.moveFrom] = c.right - 90 + 'px';
+      panel.style[dir === 'top' ? 'height' : 'width'] = c.right - 90 + 'px';
+    }
+  }
+
+  onMouseMove({ pageX, pageY }) {
+    let c = document.querySelector('.container').getBoundingClientRect();
     if (this.isMoving) {
-      console.log(e.pageX, e.pageY);
+      if (this.moveFrom === 'top') {
+        this.adjustSplitterPosition(this.moveFrom, pageY);
+      } else {
+        this.adjustSplitterPosition(this.moveFrom, pageX - c.left);
+      }
     }
   }
 
   onMouseUp(e) {
-    console.log('up');
     this.isMoving = false;
-  }
-
-  ngOnInit() {
-    document.body.addEventListener('mousemove', this.onMouseMove.bind(this));
-    document.body.addEventListener('mouseup', this.onMouseUp.bind(this));
   }
 }
--- hw6/src/app/splitter/splitter.directive.ts
+++ hw6/src/app/splitter/splitter.directive.ts
@@ -1,29 +1,48 @@
 import {
   Directive,
+  DoCheck,
   ElementRef,
   HostBinding,
   Input,
   OnInit
 } from '@angular/core';
+import { SplitterSpotDirective } from './splitter-spot.directive';
 
-type Direction = 'horizontal' | 'vertical';
+interface SplitterConfig {
+  background: string;
+  orientation: 'horizontal' | 'vertical';
+}
 
 @Directive({
   selector: '[splitter]'
 })
-export class SplitterDirective {
+export class SplitterDirective implements OnInit {
   target: HTMLElement;
 
-  @Input('splitter') direction: Direction = 'horizontal';
+  @Input('options') options: SplitterConfig;
 
-  @HostBinding('style.display') private display = 'flex';
-  @HostBinding('style.flexDirection')
-  get getDirection() {
-    return this.direction === 'vertical' ? 'column' : 'row';
+  @HostBinding('style')
+  private get styles() {
+    return Object.assign(this.target.style, {
+      height: '100%',
+      width: '100%',
+      display: 'flex',
+      backgroundColor: this.options.background,
+      flexDirection: this.options.orientation === 'vertical' ? 'column' : 'row'
+    });
   }
-  @HostBinding('class') get getCls() { return this.direction; }
+  @HostBinding('class')
+  get getCls() {
+    return this.options.orientation;
+  }
 
   constructor(private elementRef: ElementRef) {
     this.target = elementRef.nativeElement;
+  }
+
+  ngOnInit() {
+    this.target
+      .querySelector('.splitter-spot')
+      .classList.add(this.options.orientation);
   }
 }
